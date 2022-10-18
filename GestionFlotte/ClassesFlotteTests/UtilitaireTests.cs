57a99a1a6a35d5f70354b48bd5783f069cdef5de
INIAMA Timothe <timo.iniama@gmail.com>
2022-10-18 11:03:58 +0200

    parti 1 eval01

---
+++ GestionFlotte/ClassesFlotteTests/UtilitaireTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassesFlotte;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesFlotte.Tests
+{
+    [TestClass()]
+    public class UtilitaireTests
+    {
+        [TestMethod()]
+        public void ChargeUtileTest()
+        {
+            Utilitaire U = new Utilitaire("A", "electrique", 30, 3000, 2000, 300);
+            Assert.AreEqual(1000, U.ChargeUtile());
+        }
+
+        [TestMethod()]
+        public void PrevoirEntretienTest()
+        {
+            Utilitaire U = new Utilitaire("A", "electrique", 30, 3000, 2000, 300);
+            U.AjouterEntretien(new Entretien(new DateTime(2022, 05, 09), 2000, "no comment"));
+            Assert.AreEqual(false, U.PrevoirEntretien());
+
+            U.AjouterEntretien(new Entretien(new DateTime(2017, 05, 09), 2000, "no comment"));
+            Assert.AreEqual(true, U.PrevoirEntretien());
+        }
+    }
+}
